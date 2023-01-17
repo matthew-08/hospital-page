409d043c51deaf9fe4c9afa89a3ed8e601863ab9
Matthew <matthewjcrosby1@gmail.com>
2023-01-17 14:33:00 +0700

    Fix

--- src/MainCarousel/MainCarousel.jsx
+++ src/MainCarousel/MainCarousel.jsx
@@ -10,40 +10,7 @@ import 'swiper/css'
 export default function MainCarousel() {
     return (
         <>
-            <Swiper
-                spaceBetween={30}
-                slidesPerView={1.2}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
-                pagination={{
-                    clickable: true,
-                }}
-                modules={[Autoplay, Pagination]}
-                className={styles.swiper}
-                autoplay={{ delay: 10000 }}
-            >
-                <div className={styles.infoside}>
-                    <div className={styles.cont}>
-                        <p>Over 300,000 customers believe in our service.</p>
-                        <button>Book Today!</button>
-                    </div>
-                    <div className={styles.bannericon}>
-                        <img src="/img/clinic-icon1.svg" alt="" />
-                    </div>
-                </div>
-                <SwiperSlide>
-                    <img src="/img/hospital-pic1.jpg" alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="/img/hospital-pic2.jpg" alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="/img/hospital-pic3.jpg" alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="/img/hospital-pic4.jpg" alt="" />
-                </SwiperSlide>
-            </Swiper>
+            
         </>
 
     )
