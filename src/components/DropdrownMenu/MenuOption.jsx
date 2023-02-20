f605a9ae22829c9a46c2351ef92b0bcf484fc064
Matthew <matthewjcrosby1@gmail.com>
2023-02-20 16:58:00 +0700

    test

--- src/components/DropdrownMenu/MenuOption.jsx
+++ src/components/DropdrownMenu/MenuOption.jsx
@@ -4,11 +4,11 @@ import styles from './dropdown.module.css'
 
 export default function MenuOption({ img, title }) {
     return (
-        <div className={styles.menuoption}>
+       /*  <div className={styles.menuoption}>
             <div className={styles.imgcont}>
                 <img src={`${img}`} />
             </div>
             <span>{title}</span>
-        </div>
+        </div> */
     )
 }
